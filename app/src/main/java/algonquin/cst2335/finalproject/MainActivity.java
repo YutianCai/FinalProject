8c89fa70b7b6005df319e94aeb1942307a33333a
Chen Wu <wu000300@algonquinlive.com>
2023-07-19 18:17:52 -0400

    Finished part1

--- app/src/main/java/algonquin/cst2335/finalproject/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/finalproject/MainActivity.java
@@ -2,7 +2,9 @@ package algonquin.cst2335.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent bear = new Intent( MainActivity.this,BearHomepage.class);
+
+        Button bearButton = findViewById(R.id.bear);
+
+        bearButton.setOnClickListener(clk->{
+            startActivity(bear);
+        });
     }
 }
